08ccf10a8080de19a2dfe6ce78927a49bef1be5c
Fikri Firmansyah <60101886+Firmansyah-F@users.noreply.github.com>
2021-03-10 07:57:23 +0700

    Delete app directory

--- app/routers/todo.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {getAlltodos} = require ("./../controllers/todoController")
-
-router.get("/todos", getAlltodos)
-
-module.exports = {router}
