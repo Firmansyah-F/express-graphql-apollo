4f41d70d2f057f2829a47d004a57b89d4277ee77
Fikri Firmansyah <60101886+Firmansyah-F@users.noreply.github.com>
2021-03-10 07:56:52 +0700

    Delete RestApiMovie directory

--- RestApiMovie/appv1/routers/genre.js
+++
@@ -1,9 +0,0 @@
-
-const express = require("express");
-const router = express.Router();
-const genreController = require("../controllers/genreController");
-
-
-router.route("/genres").get(genreController.getAll)
-
-module.exports = { router };
--- RestApiMovie/appv1/routers/language.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const languageController = require("../controllers/languangeController");
-
-
-router.route("/languages").get(languageController.getAll)
-
-module.exports = { router };
